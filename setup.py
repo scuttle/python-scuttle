89680c694fb4642d66a3ee1f00f86e3595680a9b
Ross Williams <rossjrw@gmail.com>
2020-07-01 09:57:06 +0100

    Revert name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='scuttle-api-wrapper',
+    name='python-scuttle',
     use_scm_version=True,
     packages=['scuttle', 'scuttle.versions'],
     url="http://github.com/scuttle/python-scuttle",
